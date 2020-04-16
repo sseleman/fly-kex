28bea7d57bdf79e6bbb5f884acf76900181e3cd5
sseleman <2182446+sseleman@users.noreply.github.com>
2020-04-16 03:30:47 +0300

    Update index.js

--- coollinks/index.js
+++ coollinks/index.js
@@ -98,7 +98,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="coolinks.txt"; // add your own name here
+Typer.file="coollinks.txt"; // add your own name here
 Typer.init();
  
 var timer = setInterval("t();", 30);
